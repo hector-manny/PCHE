5e5fa4c32bab09b8c37f81ae354310e1027e7d66
hector-manny <mejia.martinez2012@hotmail.es>
2023-09-07 16:32:41 -0600

    fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
 
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+/*const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 mix.webpackConfig({
     plugins: [
@@ -12,4 +12,4 @@ mix.webpackConfig({
         }),
     ],
 });
- 
+ */
