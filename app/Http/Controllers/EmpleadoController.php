9300cfb98bb1926cdfd4a4c9c787ebadf0ff1e03
AngelMelara <angelmelara44@gmail.com>
2023-09-11 11:40:10 -0600

    importacion de modelo empleado

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -7,21 +7,18 @@ use Illuminate\Http\Request;
 use \App\Empleado;
 use Illuminate\Support\Facades\Hash;
 
-
 class EmpleadoController extends Controller
 {
-    public function allEmpleados()
-    {
+    public function allEmpleados(){
         $empleados = Empleado::all();
         return response()->json($empleados);
     }
 
-    public function store(Request $request, Area $area)
-    {
+    /*public function store(Request $request, Area $area){
 
         $request->validate([
-            'nombres' => 'required|string',
-            'apellidos' => 'required|string',
+            'nombres'=>'required|string',
+            'apellidos'=>'required|string',
             'cargo' => 'required|string',
             'correo' => 'required|email|unique:empleados',
             'area' =>  'required|exists:areas,id'
@@ -36,28 +33,21 @@ class EmpleadoController extends Controller
         ]);
 
         return response()->json(['message' => 'Empleado creado correctamente', 201]);
-    }
 
-    public function empleadosBusquedaNombre($nombre)
-    {
-        try {
+    }*/
 
+    public function empleadosBusquedaNombre(Request $request){
+        try{
+            $nombre = $request->input('nombres');
             $empleados = Empleado::where('nombres', 'LIKE', "%$nombre%")->get();
             return response()->json($empleados);
-        } catch (\Exception $e) {
+        }catch(\Exception $e){
             return response()->json(['message' => 'ocurrio un error al generar la busqueada'], 500);
         }
     }
 
-    public function actualizarEmpleados(Request $request)
-    {
+    public function actualizarEmpleados(Request $request){
         try {
-
-            $request->validate([
-                'nombres' => 'required|string',
-                'apellidos' => 'required|string',
-                'cargo' => 'required|string',
-            ]);
 
             $idEmpleado =  $request->input('id');
             $empleado = Empleado::findOrFail($idEmpleado);
@@ -69,38 +59,59 @@ class EmpleadoController extends Controller
             $empleado->nombres = $request->input('nombres');
             $empleado->apellidos = $request->input('apellidos');
             $empleado->cargo = $request->input('cargo');
-            $empleado->correo = $request->input('correo');
+            $empleado->email = $request->input('email');
+            $empleado->area_id = $request->input('area_id');
+            $empleado->dui = $request->input('dui');
+            $empleado->horario_id=$request->input('horario_id');
             $empleado->save();
 
 
+
             // Retornar una respuesta JSON con los datos actualizados
-            return response()->json(['message' => 'Empleado actualizado con éxito', 'empleado' => $empleado, 200]);
+            return response()->json(['message' => 'Empleado actualizado con éxito', 'empleado' => $empleado,200]);
         } catch (\Exception $e) {
-            return response()->json(['message' => 'Error al actualizar el registro', $e], 500);
+            return response()->json(['message' => 'Error al actualizar el registro',$e], 500);
         }
     }
 
-    public function crearEmpleado(Request $request)
-    {
+    public function crearEmpleado(Request $request){
         try {
+
+            $request->validate([
+                'nombres'=>'required|string',
+                'apellidos'=>'required|string',
+                'cargo' => 'required|string',
+                'email' => 'required|email|unique:empleados',
+                'area_id' =>  'required|exists:areas,id',
+                'email' => 'required|string',
+                'horario_id'=> 'required|exists:horarios,id',
+                'dui'=> 'required|string'
+            ]);
+
+
             $empleado = new Empleado();
             $empleado->nombres = $request->input('nombres');
             $empleado->apellidos = $request->input('apellidos');
             $empleado->cargo = $request->input('cargo');
-            $empleado->correo = $request->input('correo');
+            $empleado->email = $request->input('email');
             $empleado->area_id = $request->input('area_id');
+            $empleado->dui = $request->input('dui');
+            $empleado->horario_id=$request->input('horario_id');
             $empleado->save();
 
-            return response()->json(['message' => 'Empleado creado con éxito', 'empleado' => $empleado, 200]);
+
+            return response()->json(['message' => 'Empleado creado con éxito', 'empleado' => $empleado,200]);
         } catch (\Exception $e) {
-            return response()->json(['message' => 'Error al crear registro', $e], 500);
+            return response()->json(['message' => 'Error al crear registro',$e], 500);
         }
+
     }
 
 
-    public function eliminarEmpleados($id)
-    {
+    public function eliminarEmpleados(Request $request){
         try {
+            $id = $request->input('id');
+
             $empleados = Empleado::findOrFail($id);
             $empleados->delete();
 
@@ -110,52 +121,51 @@ class EmpleadoController extends Controller
         }
     }
 
-    public function empleadoByDui(Request $request)
-    {
-        try {
+    public function empleadoByDui(Request $request){
+        try{
 
             $duiEmpleado = $request->input('dui');
 
             $empleado = Empleado::with('area.empresa')->where('dui', 'LIKE', "%$duiEmpleado%")->first();
             return response()->json($empleado);
-        } catch (\Exception $e) {
+        }catch(\Exception $e){
             return response()->json(['ocurrio un error al obtener el empleado' => $e], 500);
         }
     }
 
     public function actualizarContrasenia(Request $request)
     {
-        try {
+    try {
 
-            $idUsuario = $request->input('idUsuario');
-            $oldPassword = $request->input('oldPassword');
-            $newPassword = $request->input('newPassword');
-            $confirmPassword = $request->input('confirmPassword');
+        $idUsuario = $request->input('idUsuario');
+        $oldPassword = $request->input('oldPassword');
+        $newPassword = $request->input('newPassword');
+        $confirmPassword = $request->input('confirmPassword');
 
-            // Retrieve the employee
-            $empleado = Empleado::find($idUsuario);
+        // Retrieve the employee
+        $empleado = Empleado::find($idUsuario);
 
-            if (!$empleado) {
-                return response()->json(['error' => 'Usuario no encontrado'], 404);
-            }
-
-            //verificar si la contraseña antigua esta correcta
-            if (!Hash::check($oldPassword, $empleado->usuario->password)) {
-                return response()->json(['error' => 'Contraseña antigua es incorrecta'], 400);
-            }
-
-            if ($newPassword != $confirmPassword) {
-                return response()->json(['error' => 'Las contraseñas no coiciden'], 400);
-            }
-
-            $empleado->usuario->update([
-                'password' => bcrypt($newPassword)
-            ]);
-
-            // Password updated successfully
-            return response()->json(['message' => 'Password updated successfully'], 200);
-        } catch (\Exception $e) {
-            return response()->json(['ocurrio un error al obtener el usuario' => $e], 500);
+        if (!$empleado) {
+            return response()->json(['error' => 'Usuario no encontrado'], 404);
         }
+
+        //verificar si la contraseña antigua esta correcta
+        if (!Hash::check($oldPassword, $empleado->usuario->password)) {
+            return response()->json(['error' => 'Contraseña antigua es incorrecta'], 400);
+        }
+
+        if ($newPassword != $confirmPassword) {
+            return response()->json(['error' => 'Las contraseñas no coiciden'], 400);
+        }
+
+        $empleado->usuario->update([
+            'password' => bcrypt($newPassword)
+        ]);
+
+        // Password updated successfully
+        return response()->json(['message' => 'Password updated successfully'], 200);
+    } catch (\Exception $e) {
+        return response()->json(['ocurrio un error al obtener el usuario' => $e], 500);
     }
+}
 }
